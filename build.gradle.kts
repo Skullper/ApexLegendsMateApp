a865da1442c8a55689a6b046e916bab20a321838
rpavliuk <pavliuk.roman@pdffiller.team>
2022-06-26 20:52:57 +0300

    Add ktlint gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,32 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "7.2.1" apply false
-    id("com.android.library").version("7.2.1").apply(false)
-    id("org.jetbrains.kotlin.android").version("1.7.0").apply(false)
+    id("com.android.library") version "7.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0" apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("0.45.2")
+        outputToConsole.set(true)
+        android.set(true)
+        ignoreFailures.set(false)
+        filter {
+            exclude { element ->
+                element.file.path.contains("generated/")
+                element.file.path.contains("test/")
+                element.file.path.contains("androidTest/")
+                element.file.path.contains("libs/")
+            }
+        }
+        disabledRules.set(setOf("no-wildcard-imports", "import-ordering"))
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        }
+    }
 }
 
 tasks.register("clean", Delete::class) {
