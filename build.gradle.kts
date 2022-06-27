c8f7a7dbd206b01a2f3fdb85edc1240a1a115b9b
rpavliuk <pavliuk.roman@pdffiller.team>
2022-06-27 17:13:06 +0300

    PR changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.7.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.7.0" apply false
-    id("com.google.devtools.ksp") version "1.7.0-1.0.6" apply false
+    id("com.google.devtools.ksp") version "1.7.0-1.0.6" apply false //required for moshi
 //    id("com.gitlab.stfs.gradle.dependency-graph-plugin") version "0.4"
 }
 
